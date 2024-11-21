fbe6216f178eb1bfac533ee4265b8fd5a0546fef
Admin <Admin@DESKTOP-2BRL2N8>
2024-11-21 14:37:50 +0530

    Add project files.

---
+++ MVCApplication/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApplication.Models
+{
+    public class Product
+    {
+        public long ProductID { get; set; }
+        public string? Name { get; set; } = null!;
+        public string Category { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+    }
+}
